438a194535f447e0d331f7dca21acba7758efabc
giuseppemaisto <gmaisto98@gmail.com>
2022-11-30 16:35:12 +0100

    inserito generatore numero utente e pc

---
+++ dadi/js/script.js
@@ -0,0 +1,7 @@
+//1 - genero numero casuale per utente 
+let num_utente = Math.floor(Math.random() * 6) + 1;
+console.log("il tuo numero è " + num_utente)
+
+//2 - genero numero casuale per pc 
+let num_pc = Math.floor(Math.random() * 6) + 1;
+console.log("il numero del pc è " + num_pc)
