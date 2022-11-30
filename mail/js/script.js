c5687b0834d2ce0cf9ca4d47d3fc21af02f814f5
giuseppemaisto <gmaisto98@gmail.com>
2022-11-30 15:29:29 +0100

    esercizio mail CORRETTO con stampa in console

--- mail/js/script.js
+++ mail/js/script.js
@@ -10,20 +10,29 @@
  //3 - controllo se mail inserita si trova in array 
  //3.1 - definisco variabile booleana flag = false 
 
- const flag = false;
+ let flag = false;
 
  //3.2- definisco ciclo for con i minore lunghezza array
  // 3.3 - confronto mail inserita con mail in array 
  //3.4 se mail inserita è presente in array, variabile flag= true 
  for(let i = 0; i<elenco_mail.length; i++){
-    if(mail == elenco_mail[i] ){
-        const flag = true;
+    if(elenco_mail[i] == mail ){
+         flag = true;
 
     // 4.1 SE variabile flag = true stampo " msil presente in elenco"
-        console.log("la tua mail è presente in elenco  ")
-    }
+}
+}
+if(flag){
+
+    console.log("la tua mail è presente in elenco  ")
+}
+  else{
     
- }
+      console.log("la tua mail non è presente in elenco ")
+  }
+
 
  // ALTRIMENTI stampo " mail non presente in elenco"
- console.log("la tua mail non è presente in elenco ")
+
+
+ document.getElementById('esito_mail').innerHTML;
