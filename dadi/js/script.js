1d87223daf7b98382283c4756dc4d8829ad394f2
giuseppemaisto <gmaisto98@gmail.com>
2022-11-30 16:41:48 +0100

    esercizio dadi completo con stampa in console

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -5,3 +5,17 @@ console.log("il tuo numero è " + num_utente)
 //2 - genero numero casuale per pc 
 let num_pc = Math.floor(Math.random() * 6) + 1;
 console.log("il numero del pc è " + num_pc)
+
+let esito;
+if(num_utente > num_pc){
+    esito = "hai vinto ";
+    console.log(esito);
+}
+else if( num_utente == num_pc){
+    esito = "pari";
+    console.log(esito);
+}
+else {
+    esito = "hai perso";
+    console.log(esito);
+}
