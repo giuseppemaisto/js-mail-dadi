037978d0c1942e612beb0a8fee523d3f7f844568
giuseppemaisto <gmaisto98@gmail.com>
2022-11-30 14:24:08 +0100

    esercizio mail: definito array

---
+++ mail/js/script.js
@@ -0,0 +1,8 @@
+//1 - chiedo all'utente di inserire la sua mail 
+ let mail = prompt("inserisci mail ")
+ console.log(mail)
+
+ //2 - definisco array con elenco di mail 
+
+ const elenco_mail = ['gmaisto98@gmail.com', 'pincopallo@gmail.com', 'pallopinco@gmail.com', 'tiziocaio@gmail.com', 'pincopallo@libero.it', 'giuseppe@gmail.com']
+ console.log(elenco_mail)
