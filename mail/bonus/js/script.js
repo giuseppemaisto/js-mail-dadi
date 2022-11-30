2e3a26975740eafee54bd0d3abe96ddcefc06b3e
giuseppemaisto <gmaisto98@gmail.com>
2022-11-30 17:12:18 +0100

    aggiunto bonus mail

---
+++ mail/bonus/js/script.js
@@ -0,0 +1,43 @@
+//1 - chiedo all'utente di inserire la sua mail 
+const mail = document.getElementById("mail").value
+console.log(mail)
+
+const BottoneConfronta = document.getElementById("confronta")
+
+BottoneConfronta.addEventListener("click", function(){
+
+
+//2 - definisco array con elenco di mail 
+
+const elenco_mail = ['gmaisto98@gmail.com', 'pincopallo@gmail.com', 'pallopinco@gmail.com', 'tiziocaio@gmail.com', 'pincopallo@libero.it', 'giuseppe@gmail.com']
+console.log(elenco_mail)
+
+//3 - controllo se mail inserita si trova in array 
+//3.1 - definisco variabile booleana flag = false 
+let esito;
+let flag = false;
+
+//3.2- definisco ciclo for con i minore lunghezza array
+// 3.3 - confronto mail inserita con mail in array 
+//3.4 se mail inserita è presente in array, variabile flag= true 
+for(let i = 0; i<elenco_mail.length; i++){
+   if(elenco_mail[i] == mail ){
+        flag = true;
+
+   // 4.1 SE variabile flag = true stampo " msil presente in elenco"
+}
+}
+if(flag){
+   esito = "la tua mail è presente in elenco "
+   console.log("la tua mail è presente in elenco  ")
+}
+ else{
+     
+      // ALTRIMENTI stampo " mail non presente in elenco"
+   esito = "la tua mail non è presente in elenco "
+     console.log("la tua mail non è presente in elenco ")
+ }
+
+ document.getElementById('esito_mail').innerHTML= esito;
+
+})
--- mail/js/script.js
+++ mail/js/script.js
@@ -9,7 +9,7 @@
  
  //3 - controllo se mail inserita si trova in array 
  //3.1 - definisco variabile booleana flag = false 
-
+ let esito;
  let flag = false;
 
  //3.2- definisco ciclo for con i minore lunghezza array
@@ -23,16 +23,16 @@
 }
 }
 if(flag){
-    
+    esito = "la tua mail è presente in elenco "
     console.log("la tua mail è presente in elenco  ")
 }
   else{
       
        // ALTRIMENTI stampo " mail non presente in elenco"
-    
+    esito = "la tua mail non è presente in elenco "
       console.log("la tua mail non è presente in elenco ")
   }
 
 
 
- document.getElementById('esito_mail').innerHTML
+ document.getElementById('esito_mail').innerHTML= esito;
